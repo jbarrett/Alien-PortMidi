a62f5e5a1c792edc76cb33f4b37496966cf35fd3
John Barrett <john@jbrt.org>
2021-09-14 11:24:12 +0100

    Initial commit

---
+++ lib/Alien/PortMidi.pm
@@ -0,0 +1,15 @@
+use strict;
+use warnings;
+package Alien::PortMidi;
+
+# ABSTRACT: Install the Mixxx fork of PortMidi
+
+our $VERSION = '0.00';
+
+use parent qw/ Alien::Base /;
+
+# ALIEN SYNOPSIS
+# ALIEN DESCRIPTION
+# ALIEN SEE ALSO
+
+1;

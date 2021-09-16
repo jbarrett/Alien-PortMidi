94b73bba32fc3f91a63ac5682c857324486b261b
John Barrett <john@jbrt.org>
2021-09-16 16:26:53 +0100

    Improving Windows build support.

--- lib/Alien/PortMidi.pm
+++ lib/Alien/PortMidi.pm
@@ -8,6 +8,18 @@ our $VERSION = '0.00';
 
 use parent qw/ Alien::Base /;
 
+BEGIN {
+    # On *nix we can set RPATH to $ORIGIN so portmidi can find porttime
+    # in the same dir. Windows DLL search path does not offer similar.
+    # Explicitly loading porttime before portmidi obviates the need to
+    # worry about the Windows search path.
+    if ( $^O eq 'MSWin32' ) {
+        require DynaLoader;
+        DynaLoader::dl_load_file($_)
+          for grep { /porttime/ } __PACKAGE__->dynamic_libs;
+    }
+}
+
 # ALIEN SYNOPSIS
 # ALIEN DESCRIPTION
 # ALIEN SEE ALSO
--- t/alien_portmidi.t
+++ t/alien_portmidi.t
@@ -3,7 +3,7 @@ use Test::Alien;
 use Alien::PortMidi;
 
 alien_ok 'Alien::PortMidi';
-ffi_ok { symbols => [ 'Pt_Start', 'Pm_GetErrorText' ], api => 1 }, with_subtest {
+ffi_ok { symbols => [ 'Pm_GetErrorText' ], api => 1 }, with_subtest {
     my($ffi) = @_;
     my $Pm_GetErrorText = $ffi->function( Pm_GetErrorText => [ 'int' ] => 'string' );
     is $Pm_GetErrorText->call(-10000), "PortMidi: `Host error'", 'Pm_GetErrorText works';
